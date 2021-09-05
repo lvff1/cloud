62395e874168c363203c1796fb1ec9ef645ada92
大吉 <1134107721@qq.com>
2021-09-05 21:10:58 +0800

    更新完Eureka、Ribbon、Nacos的相关内容，包括Nacos负载均衡、Nacos热更新部分

--- 01-cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ 01-cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,18 +1,33 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
 @RequestMapping("/user")
+@RefreshScope
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PatternProperties patternProperties;
+
+    @GetMapping("prop")
+    public PatternProperties TestShareValue(){
+        return patternProperties;
+    }
 
     /**
      * 路径： /user/110
@@ -23,5 +38,16 @@ public class UserController {
     @GetMapping("/{id}")
     public User queryById(@PathVariable("id") Long id) {
         return userService.queryById(id);
+    }
+
+    @Value("${pattern.dateformat}")
+    private String dateformat;
+
+    @GetMapping("now")
+    public String now(){
+        System.out.println(dateformat);
+        //TODO 下面演示两种方式实现热更新：第一种是使用Value注解(被注释掉了)；第二种是使用配置文件(推荐)
+        //return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
     }
 }
